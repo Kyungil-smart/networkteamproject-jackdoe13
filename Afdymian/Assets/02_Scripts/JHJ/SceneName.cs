b3ef0538fd9bba9a4738c81975c219b344737811
hjchun78-cmd <hjchun78@gmail.com>
2026-05-08 16:27:40 +0900

    [기능] Scene매니저 나누기
    기본 타이틀 매니저와 씬을 옮길 때 매니저를 나눔

---
+++ Afdymian/Assets/02_Scripts/JHJ/SceneName.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class SceneNames
+{
+    public const string Title1 = "Title_Scene1";
+    public const string Title = "TitleScene";
+    public const string Lobby = "LobbyScene";
+    public const string Game = "GameScene";
+}
